b09639ddf733b8879c1c37e0d7bd94a02be751bb
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2020-07-21 10:10:43 -0400

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -334,16 +334,19 @@ pub fn move_position(old_position: &Point, dir: Direction) -> Point {
 
 event_reader_res!(InputDriverRes, VirtualKeyCode);
 
-system!(InputDriver<E: Clone + Send + Sync + 'static>, |keymap: Read<'a, HashMap<VirtualKeyCode, E>>, 
-        inputs: Read<'a, EventChannel<VirtualKeyCode>>,
-        events: Write<'a, EventChannel<E>>,
-        reader: WriteExpect<'a, InputDriverRes>| {
-    for i in inputs.read(&mut reader.0) {
-        if let Some(e) = keymap.get(i) {
-            events.single_write(e.clone());
+system!(
+    InputDriver<E: Clone + Send + Sync + 'static>,
+    |keymap: Read<'a, HashMap<VirtualKeyCode, E>>,
+     inputs: Read<'a, EventChannel<VirtualKeyCode>>,
+     events: Write<'a, EventChannel<E>>,
+     reader: WriteExpect<'a, InputDriverRes>| {
+        for i in inputs.read(&mut reader.0) {
+            if let Some(e) = keymap.get(i) {
+                events.single_write(e.clone());
+            }
         }
     }
-});
+);
 
 pub fn render_sprites<'a>(
     ctx: &mut BTerm,
