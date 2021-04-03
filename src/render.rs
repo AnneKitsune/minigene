a7e190e463547dac6a8b3129032671839911ae69
jojolepro <jojolepro@jojolepro.com>
2021-04-02 21:51:40 -0400

    try setting the camera check to zero.

--- src/render.rs
+++ src/render.rs
@@ -31,8 +31,8 @@ pub fn render_ascii<'a>(
             if camera.size.x > 0 && camera.size.y > 0 && position_inside_rect(
                 pos.x,
                 pos.y,
-                camera.position.x,
-                camera.position.y,
+                0,
+                0,
                 camera.size.x as u32,
                 camera.size.y as u32,
             ) {
@@ -66,8 +66,8 @@ pub fn render_sprites<'a>(
         if camera.size.x > 0 && camera.size.y > 0 && position_inside_rect(
             pos.x,
             pos.y,
-            camera.position.x,
-            camera.position.y,
+            0,
+            0,
             camera.size.x as u32,
             camera.size.y as u32,
         ) {
