9663974a7f80c5876811bb2e627c2ccbcf11547a
AnneKitsune <annekitsunefox@gmail.com>
2025-07-18 13:12:17 -0400

    fix goto_straight

--- src/systems/goto_straight.rs
+++ src/systems/goto_straight.rs
@@ -1,13 +1,21 @@
 use crate::*;
 
 /// Moves an entity one tile towards the target position, regardless of collisions.
+///
+/// # Panics
+/// This function panics if any entity is encountered that has a `GotoStraight` component without
+/// a `Point` component, or vice versa. This situation is considered unrecoverable and indicates a
+/// problem in entity setup or component management.
+///
+/// # Errors
+/// FIXME: Currently, this function does not return any errors. In the future, we should handle component mismatches more gracefully.
 pub fn goto_straight_system(
     gotos: &Components<GotoStraight>,
     positions: &mut Components<Point>,
 ) -> SystemResult {
     for (p, goto) in join!(&mut positions && &gotos) {
-        let p = p.unwrap();
-        let goto = goto.unwrap();
+        let p = p.expect("Expected Point component to exist for GotoStraight entity");
+        let goto = goto.expect("Expected GotoStraight component to exist for Point entity");
         for _i in 0..(goto.speed as usize) {
             let delta_x = goto.target.x - p.x;
             let delta_y = goto.target.y - p.y;
