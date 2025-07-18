20803f25f1c55f065068d6e70fb0e219935881f5
AnneKitsune <annekitsunefox@gmail.com>
2025-07-18 13:54:13 -0400

    fix src/resources

--- src/resources.rs
+++ src/resources.rs
@@ -12,7 +12,7 @@ pub struct CollisionResource {
 
 impl CollisionResource {
     /// Checks whether the position is located inside of the `CollisionMap`.
-    pub fn is_inside(&self, p: &Point) -> bool {
+    pub const fn is_inside(&self, p: &Point) -> bool {
         position_inside_rect(
             p.x - self.position.x,
             p.y - self.position.y,
@@ -22,8 +22,8 @@ impl CollisionResource {
             self.map.size().1,
         )
     }
-    /// Check is_inside before calling this.
-    pub fn relative_point(&self, p: &Point) -> (u32, u32) {
+    /// Check `is_inside` before calling this.
+    pub const fn relative_point(&self, p: &Point) -> (u32, u32) {
         (
             (p.x - self.position.x) as u32,
             (p.y - self.position.y) as u32,
@@ -47,7 +47,7 @@ pub struct RNG {
 
 impl Default for RNG {
     fn default() -> Self {
-        RNG {
+        Self {
             rng: oorandom::Rand32::new(1),
         }
     }
