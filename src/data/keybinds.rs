ebe8499f836d1075d766cc514e15367bb316d0b2
AnneKitsune <annekitsunefox@gmail.com>
2025-07-18 13:58:25 -0400

    fix keybinds

--- src/data/keybinds.rs
+++ src/data/keybinds.rs
@@ -1,12 +1,16 @@
 use crossterm::event::KeyCode;
 
+/// A mapping from a keycode to an event.
 pub struct Keybind<E> {
+    /// The keycode
     pub key: KeyCode,
+    /// The associated event that gets generated once the keyboard key associated with the keycode is pressed.
     pub event: E,
 }
 
 impl<E> Keybind<E> {
-    pub fn new(key: KeyCode, event: E) -> Self {
-        Keybind { key, event }
+    /// Creates a new `Keybind` with the provided keycode and associated event.
+    pub const fn new(key: KeyCode, event: E) -> Self {
+        Self { key, event }
     }
 }
