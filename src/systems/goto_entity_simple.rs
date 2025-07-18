fd81509abff0cb760fc3fa8d6c5bbff7de04caa6
AnneKitsune <annekitsunefox@gmail.com>
2025-07-18 13:15:42 -0400

    fix goto entity simple system

--- src/systems/goto_entity_simple.rs
+++ src/systems/goto_entity_simple.rs
@@ -1,6 +1,15 @@
 use crate::*;
 
 /// Moves an entity one tile towards the targeted entity, regardless of collisions.
+///
+/// # Errors
+/// This function returns `Ok(())` under all conditions since it does not generate any errors. It may only panic in case of internal inconsistency.
+///
+/// # Panics
+/// This function panics if:
+///   - An entity that has a `GotoEntity` and `Point` component (as found in the first pass) is missing the `Point` component in the second pass (which should be impossible).
+///   - The `Entity` id from the first pass is `None` (which should be impossible).
+///   - The `GotoEntity` component from the first pass is `None` (which should be impossible).
 pub fn goto_entity_simple_system(
     entities: &Entities,
     gotos: &Components<GotoEntity>,
@@ -8,11 +17,15 @@ pub fn goto_entity_simple_system(
 ) -> SystemResult {
     let mut v = vec![];
     for (e, _, goto) in join!(&entities && &positions && &gotos) {
-        v.push((e.unwrap(), goto.unwrap().entity, goto.unwrap().speed));
+        let entity_id = e.expect("Expected an entity id to exist");
+        let goto_component = goto.expect("Expected GotoEntity component to exist");
+        v.push((entity_id, goto_component.entity, goto_component.speed));
     }
     for (e, t, speed) in v {
         if let Some(target) = positions.get(t).copied() {
-            let p = positions.get_mut(e).unwrap();
+            let p = positions
+                .get_mut(e)
+                .expect("Entity to move must have a position component");
             for _i in 0..(speed as usize) {
                 let delta_x = target.x - p.x;
                 let delta_y = target.y - p.y;
