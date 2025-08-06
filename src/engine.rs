4e5e3409cfb0c80257e89694ace615f66f0990e4
AnneKitsune <annekitsunefox@gmail.com>
2025-08-05 22:06:59 -0400

    add thread profiler

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,5 @@
 use crate::resources::EngineRunning;
+use thread_profiler::profile_scope;
 use world_dispatcher::{Dispatcher, DispatcherBuilder, System, World};
 
 fn build_dispatcher(world: &mut World, systems: Vec<System>) -> Dispatcher {
@@ -15,6 +16,11 @@ fn build_dispatcher(world: &mut World, systems: Vec<System>) -> Dispatcher {
 ///
 /// Panics if the initial `EngineRunning` resource is not present after setup.
 pub fn run(init_systems: Vec<System>, run_systems: Vec<System>) {
+    #[cfg(feature = "thread_profiler")]
+    thread_profiler::register_thread_with_profiler();
+
+    profile_scope!("engine::run");
+
     let mut world = World::default();
     {
         let mut running = world.get_mut_or_default::<EngineRunning>();
@@ -24,9 +30,13 @@ pub fn run(init_systems: Vec<System>, run_systems: Vec<System>) {
     let mut init_dispatcher = build_dispatcher(&mut world, init_systems);
     let mut run_dispatcher = build_dispatcher(&mut world, run_systems);
 
-    init_dispatcher.run_seq(&world);
+    {
+        profile_scope!("engine init systems");
+        init_dispatcher.run_seq(&world);
+    }
 
     while world.get::<EngineRunning>().running {
+        profile_scope!("engine run systems");
         run_dispatcher.run_seq(&world);
     }
 }
